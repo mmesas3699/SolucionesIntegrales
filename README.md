cd3d5fc3768f99c70ff3dfd512d8bd7c469dce0b
Miguel Mesa <mmesas369@gmail.com>
2019-08-14 15:52:41 -0500

    Modifico el README y quito espacios en config_app

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 # Instalación de dependencias
 
+> apt-get install python3-venv
+
 > Para Python_MySQL:
 
     $ sudo apt-get install python-pip python-dev libmysqlclient-dev
--- config_app.py
+++ config_app.py
@@ -3,33 +3,33 @@ import os
 
 
 class Config(object):
-	DEBUG = False
-	TESTING = False
+    DEBUG = False
+    TESTING = False
 
 
 class ProductionConfig(Config):
 
-	DB_NAME = os.environ.get('DB_NAME')
-	DB_USER = os.environ.get('DB_USER')
-	DB_PASS = os.environ.get('DB_PASSWORD')
+    DB_NAME = os.environ.get('DB_NAME')
+    DB_USER = os.environ.get('DB_USER')
+    DB_PASS = os.environ.get('DB_PASSWORD')
 
-	WTF_CSRF_ENABLE = True
-	SECRET_KEY = os.environ.get('SECRET_KEY')
+    WTF_CSRF_ENABLE = True
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
-	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@127.0.0.1:33060/{}?charset=utf8mb4'.format(DB_USER, DB_PASS, DB_NAME)
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@127.0.0.1:33060/{}?charset=utf8mb4'.format(DB_USER, DB_PASS, DB_NAME)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-	MAIL_SERVER = 'smtp.gmail.com'
-	MAIL_PORT = 465
-	MAIL_USE_SSL = True
-	MAIL_USE_TLS = False
-	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-	MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USE_TLS = False
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
 
 
 class DevelopConfig(Config):
-	
+
     DEBUG = True
 
     DB_NAME = os.environ.get('DB_NAME')
